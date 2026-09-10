a9c565c0e3a0d0352d7e3d8b186f5a02f24ba544
mehedynoman11 <mehedynoman11@gmail.com>
2026-09-11 00:05:28 +0600

    Navbar completed

---
+++ src/components/Nav.tsx
@@ -0,0 +1,22 @@
+import Logo from "../assets/logo-text.png"
+
+const Nav = () => {
+    return (
+        <nav className="flex justify-between items-center gap-4 container mx-auto max-w-[1200px] py-4">
+            <img src={Logo} alt="" />
+            <ul className="hidden sm:flex gap-4">
+                <li>Home</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div>
+                <button className="btn rounded-full">Sign In</button>
+                <button className="btn rounded-full text-white bg-[#D91B7E]">Sign Up</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
